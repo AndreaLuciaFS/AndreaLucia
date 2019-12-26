9dbdb5999a4f86963b83a6e527f2ef049015d80f
AndreaLuciaFS <58514304+AndreaLuciaFS@users.noreply.github.com>
2019-12-26 13:23:33 +0100

    Update main.js

--- guiones/main.js
+++ guiones/main.js
@@ -1,8 +1,14 @@
-function cambiar(id)
-{
-imagensrc=document.getElementById(id).src;
-if(imagensrc==otra)
-{
-imagensrc=una;
-}
-}
+var dogEl = document.getElementById("perro");
+  
+  var startTime = new Date().getTime();
+  var walkTheDog = function() {
+      var currTime = new Date().getTime();
+      var secondsElapsed = ((currTime - startTime)/1000);
+      
+    var newRight = (50 + ((currTime - startTime)/1000)*30);
+    dogEl.style.right = newRight + "px";
+    window.requestAnimationFrame(walkTheDog)
+  };
+  
+  
+  walkTheDog();
