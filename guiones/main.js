b2451eb472b52fcac5cc704e3ff24f27bd2a03e9
AndreaLuciaFS <58514304+AndreaLuciaFS@users.noreply.github.com>
2019-12-26 12:35:05 +0100

    Update main.js

--- guiones/main.js
+++ guiones/main.js
@@ -1,27 +1,8 @@
-var imagenes=Array("imágenes/Imagen perro adopción.jpg","imágenes/perro feliz adopción.jpg");
-	var imagenVisible=0;
- 
-	function cambiar(img)
-	{
-		imagenVisible++;
-		if(imagenVisible>=imagenes.length)
-		{
-			imagenVisible=0;
-		}
-		img.src=imagenes[imagenVisible];
-		cargarSiguienteImagen();
-	}
- 
-	function cargarSiguienteImagen()
-	{
-		if((imagenVisible+1)<imagenes.length)
-		{
-			console.log(imagenVisible+1);
-			var imgTmp=new Image();
-			imgTmp.src=imagenes[imagenVisible+1];
-		}
-	}
- 
-	window.onload=function() {
-		cargarSiguienteImagen();
-	}
+function cambiar(id)
+{
+imagensrc=document.getElementById(id).src;
+if(imagensrc==otra)
+{
+imagensrc=una;
+}
+}
