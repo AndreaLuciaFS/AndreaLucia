5e19ac59c1a04f726aeff18d141803db2c28e860
AndreaLuciaFS <58514304+AndreaLuciaFS@users.noreply.github.com>
2019-12-26 12:13:35 +0100

    Update main.js

--- guiones/main.js
+++ guiones/main.js
@@ -1,1 +1,32 @@
-
+var imagenes=Array("imágenes/","img_2.jpg","img_3.jpg");
+	var imagenVisible=0;
+ 
+	// Función que cambia la imagen actual por la siguiente
+	function cambiar(img)
+	{
+		imagenVisible++;
+		if(imagenVisible>=imagenes.length)
+		{
+			imagenVisible=0;
+		}
+		img.src=imagenes[imagenVisible];
+		cargarSiguienteImagen();
+	}
+ 
+	/**
+	 * Esta función carga la siguiente imagen para no tener que esperar su carga
+	 * en el momento de mostrarla.
+	 */
+	function cargarSiguienteImagen()
+	{
+		if((imagenVisible+1)<imagenes.length)
+		{
+			console.log(imagenVisible+1);
+			var imgTmp=new Image();
+			imgTmp.src=imagenes[imagenVisible+1];
+		}
+	}
+ 
+	window.onload=function() {
+		cargarSiguienteImagen();
+	}
